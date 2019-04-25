2448c08e64c46b916267632bf6b01b1e0719d44f
jgp <jgp@cscs.ch>
2019-04-25 15:39:53 +0200

    alias

--- sph/miniapp/reframe_res.sh
+++ sph/miniapp/reframe_res.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 weak="F"
-strong="T"
+strong="F"
+cpubind="T"
 
  #    #  ######    ##    #    #
  #    #  #        #  #   #   #
@@ -48,6 +49,40 @@ echo -n "$fn "
 dn=`dirname $in`
 mpit=`echo $dn |cut -d_ -f4|sed "s-mpi--"`
 cn=`echo $mpit |awk '{print $0/12}'`
+ompt=`echo $dn |cut -d_ -f5 |cut -d- -f1 |sed "s-omp--"`
+compilerv=`echo $dn |awk -F_ '{print $3}'`
+cpubind=`echo $dn|awk -F-cc '{print $2}'|cut -d_ -f1`
+if [ -z $cpubind ]; then cpubind="empty"; fi
+
+steps=`grep '=== Total time for iteration' $in |wc -l`
+elapsedtime=`grep '=== Total time for iteration' $in |head -$xsteps |awk '{s=s+$6}END{print s}'`
+timeperstep=`grep '=== Total time for iteration' $in |head -$xsteps |awk '{s=s+$6}END{print s/NR}'`
+
+echo "@$mpit @$ompt @$steps @$xsteps @$cn @$elapsedtime @$timeperstep @$compilerv @$cpubind" |tr -d " "
+
+# echo "@$mpit @$ompt @$timeperstep @$compilerv @$cpubind" |tr -d " "
+# echo $mpit $ompt $timeperstep $compilerv $cpubind
+fi
+
+
+  ####   #####   #    #  #####      #    #    #  #####
+ #    #  #    #  #    #  #    #     #    ##   #  #    #
+ #       #    #  #    #  #####      #    # #  #  #    #
+ #       #####   #    #  #    #     #    #  # #  #    #
+ #    #  #       #    #  #    #     #    #   ##  #    #
+  ####   #        ####   #####      #    #    #  #####
+# --- cpubind:
+if [ $cpubind = "T" ];then
+xsteps=35
+# xsteps=18
+# xsteps=5
+in=$1
+
+fn=`basename $in`
+echo -n "$fn "
+dn=`dirname $in`
+mpit=`echo $dn |cut -d_ -f4|sed "s-mpi--"`
+cn=1
 ompt=`echo $dn |cut -d_ -f5 |cut -d- -f1 |sed "s-omp--"`
 compilerv=`echo $dn |awk -F_ '{print $3}'`
 cpubind=`echo $dn|awk -F-cc '{print $2}'|cut -d_ -f1`
