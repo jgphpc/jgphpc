34d275c663cc8b39fba1c1c585fca98c244c7edc
jgp <jgp@cscs.ch>
2019-06-21 20:52:00 +0200

    index

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+# READTHEDOCS
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    'collapse_navigation': True,
+    'display_version': True,
+    'navigation_depth': 5,
+    #     # 'canonical_url': True,
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
