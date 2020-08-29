2930d9f5421f7746d09e2094f36680be9a6203db
jgp <jgp@cscs.ch>
2020-08-29 12:44:20 +0200

    fix

--- git/git_compare.sh
+++ git/git_compare.sh
@@ -19,11 +19,16 @@ if [ $log_size -lt 5 ] ;then
 else
     back_to=10
 fi
+echo "# log_size=$log_size"
 back_to_m1=`expr $back_to - 1`
 git log --pretty=oneline $srcf |head -n $back_to |$myrev |awk '{print $1}' > $tmp1
-tail -n $back_to_m1 $tmp1 > $tmp2
-paste $tmp1 $tmp2 \
-|awk -v q=$srcf '{ss="git diff --no-ext-diff -w "substr($1,1,7)" "substr($2,1,7)" "q ;print ss}' \
-|head -$back_to_m1 # > $tmp3
+
+previous='# '
+while read line ;do
+    # echo "$line $previous"
+    echo "$previous $line"
+    previous="$line"
+done < $tmp1 |egrep -v "^#" > $tmp2
+awk -v q=$srcf '{ss="git diff --no-ext-diff -w "substr($1,1,7)" "substr($2,1,7)" "q ;print ss}' $tmp2
 
 rm -f $tmp1 $tmp2
